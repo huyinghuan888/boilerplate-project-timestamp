b0fef6126444c06bcc2d60652a0bdbbf9a7bcfe5
Replit user <>
2022-12-05 14:53:39 +0800

    恢复端口修改

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ app.get("/api/:date?", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
